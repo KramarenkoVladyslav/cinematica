56c33b2da28ddc9d5a45e5d8a781b71a40389a41
Vladyslav <93004336+KramarenkoVladyslav@users.noreply.github.com>
2025-06-15 16:32:23 +0300

    Added transaction for sumbit_review

--- reviews/views.py
+++ reviews/views.py
@@ -4,30 +4,36 @@ from .models import Review
 from movies.models import Movie
 from .forms import ReviewForm
 from django.contrib import messages
+from django.db import transaction
 
 
 @login_required
 def submit_review(request, movie_id):
     movie = get_object_or_404(Movie, id=movie_id)
 
-    existing_review = Review.objects.filter(user=request.user, movie=movie).first()
-    if existing_review:
-        messages.error(
-            request,
-            "You have already submitted a review for this movie. Please delete your existing review before submitting a new one.",
-        )
-        return redirect("movie_detail", movie_id=movie.id)
-
     if request.method == "POST":
         form = ReviewForm(request.POST)
         if form.is_valid():
-            Review.objects.create(
-                user=request.user,
-                movie=movie,
-                rating=form.cleaned_data["rating"],
-                review_text=form.cleaned_data["review_text"],
-            )
-            messages.success(request, "Your comment has been successfully added.")
+            with transaction.atomic():
+                existing_review = Review.objects.select_for_update().filter(
+                    user=request.user, movie=movie
+                ).first()
+
+                if existing_review:
+                    messages.error(
+                        request,
+                        "You have already submitted a review for this movie. Please delete your existing review before submitting a new one.",
+                    )
+                    return redirect("movie_detail", movie_id=movie.id)
+
+                Review.objects.create(
+                    user=request.user,
+                    movie=movie,
+                    rating=form.cleaned_data["rating"],
+                    review_text=form.cleaned_data["review_text"],
+                )
+                messages.success(request, "Your comment has been successfully added.")
+
     return redirect("movie_detail", movie_id=movie.id)
 
 
