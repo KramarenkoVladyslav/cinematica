92fe304604d5cf100440b96d3656d209be9a446b
Vladyslav <93004336+KramarenkoVladyslav@users.noreply.github.com>
2025-06-16 11:54:23 +0300

    Validate email in forms

--- accounts/forms.py
+++ accounts/forms.py
@@ -1,11 +1,21 @@
 from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from .models import User
+from django.core.validators import RegexValidator
 
 
 class SignupForm(UserCreationForm):
     email = forms.EmailField(
-        required=True, label="Email", max_length=255, help_text="Required."
+        required=True,
+        label="Email",
+        max_length=255,
+        help_text="Required.",
+        validators=[
+            RegexValidator(
+                regex=r'^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$',
+                message='Enter a valid email address.',
+            )
+        ],
     )
 
     class Meta:
