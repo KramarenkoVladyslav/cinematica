122c53d948014fa44c9108dae6009d9b340b89a7
Vladyslav Kramarenko <vkramare@cisco.com>
2025-09-04 22:11:58 +0300

    Replace file logging with console logging in settings.py for improved debugging

--- cinematica/settings.py
+++ cinematica/settings.py
@@ -125,23 +125,13 @@ LOGGING = {
     "version": 1,
     "disable_existing_loggers": False,
     "handlers": {
-        "file": {
-            "level": "DEBUG",
-            "class": "logging.FileHandler",
-            "filename": "debug.log",
+        "console": {
+            "class": "logging.StreamHandler",
         },
     },
-    "loggers": {
-        "django": {
-            "handlers": ["file"],
-            "level": "DEBUG",
-            "propagate": True,
-        },
-        "accounts.views": {
-            "handlers": ["file"],
-            "level": "INFO",
-            "propagate": False,
-        },
+    "root": {
+        "handlers": ["console"],
+        "level": "DEBUG",
     },
 }
 
